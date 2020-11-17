675e3ed81637ad1ac84cc9440745740a1f7cc241
tyan001 <tyan001@fiu.edu>
2020-11-16 23:12:04 -0500

    added setup.py

---
+++ QuickMLMain/Setup.py
@@ -0,0 +1,10 @@
+import subprocess
+import sys
+
+if __name__ == '__main__':
+    packages = ['Tensorflow == 2.3.1', 'pyqt5 == 5.15.1','numpy == 1.18.5','pillow == 8.0.1','matplotlib = 3.2.2']
+    for package in packages:
+        install(package)
+
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
