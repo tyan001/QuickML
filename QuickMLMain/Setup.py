7ba006d9e73639ddd3ee23268b8021b2df447d0c
tyan001 <tyan001@fiu.edu>
2020-11-17 20:31:51 -0500

    fix a dependency error in setup

--- QuickMLMain/Setup.py
+++ QuickMLMain/Setup.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 if __name__ == '__main__':
-    packages = ['Tensorflow == 2.3.1', 'pyqt5 == 5.15.1','numpy == 1.18.5','pillow == 8.0.1','matplotlib = 3.2.2']
+    packages = ['Tensorflow == 2.3.1', 'pyqt5 == 5.15.1','numpy == 1.18.5','pillow == 8.0.1','matplotlib == 3.2.2', 'scipy == 1.5.4']
     for package in packages:
         install(package)
 
