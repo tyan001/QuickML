63b2ecdbf2f9184fc817c8e7ed25131dc54a5f8a
tyan001 <thonyyan@gmail.com>
2020-12-01 19:40:15 -0500

    testing

--- QuickMLMain/preprocessingGUI.py
+++ QuickMLMain/preprocessingGUI.py
@@ -10,7 +10,10 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import preprocessingFunctions
-
+import os
+import multiprocessing as mp
+import shutil
+import random
  
 class Ui_MainWindow(object):
     def __init__(self):
@@ -149,13 +152,85 @@ class Ui_MainWindow(object):
         preprocessingFunctions.multi_process_dataset(project_name, labels, paths)
         self.finish_dialog()
 
+    def make_data_directories(self,dir_name, labels):
+        # dir_name - Top level directory name
+        # labels - labels names. (e.g. cats, dogs, horses)
+        try:
+            os.mkdir(dir_name)  # making top level directory
+        except OSError as e:
+            print(e.filename, 'exists already')
+            pass
 
-if __name__ == "__main__":
-    import sys
+        train_path = os.path.join(dir_name, 'training')
+        testing_path = os.path.join(dir_name, 'testing')
 
-    app = QtWidgets.QApplication(sys.argv)
-    MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    sys.exit(app.exec_())
+        try:
+            os.mkdir(train_path)  # making training subdirectory
+        except FileExistsError as e:
+            print(e.filename, 'exists already')
+            pass
+
+        try:
+            os.mkdir(testing_path)  # making testing subdirectory
+        except FileExistsError as e:
+            print(e.filename, 'exists already')
+            pass
+
+        for i in range(len(labels)):  # making training labels and testing labels subdirectory
+            try:
+                os.mkdir(os.path.join(train_path, str(labels[i])))
+            except OSError as e:
+                print(e.filename, 'exists already')
+                pass
+
+            try:
+                os.mkdir(os.path.join(testing_path, str(labels[i])))
+            except OSError as e:
+                print(e.filename, 'exists already')
+                pass
+
+    def split_data(self, SOURCE, TRAINING, TESTING, SPLIT_SIZE):
+        # SOURCE - Path location where the images are located
+        # TRAINING - Path location where the training data will be moved to
+        # TESTING - Path location where the testing data will be moved to
+        # SPLIT_SIZE = The amount of images going to the training dataset by %.
+        # (e.g. SPLIT_SIZE = .9: total images = 100, 90 images goes to training directory, 10 images goes to test directory)
+
+        testing_file_names = []
+        file_names = os.listdir(SOURCE)  #
+        t = round(len(file_names) * (1 - SPLIT_SIZE))
+        random.shuffle(file_names)
+        for i in range(int(t)):
+            testing_file_names.append(file_names[i])  # adding the file names for the testing data set
+            file_names.remove(file_names[i])  # removing the testing data file names
+
+        for jpg in range(
+                len(testing_file_names)):  # using the testing file names to move from source to testing directory
+            shutil.copy2(os.path.join(SOURCE, testing_file_names[jpg]), TESTING)
+
+        for jpg in range(len(file_names)):  # # using the file names to move from source to training directory
+            shutil.copy2(os.path.join(SOURCE, file_names[jpg]), TRAINING)
+
+    def make_dataset(self,new_dir, labels, src_directories):
+        self.make_data_directories(new_dir, labels)
+        for i in range(len(labels)):
+            split_data(SOURCE=src_directories[i],
+                       TRAINING=os.path.join(new_dir, 'training', labels[i]),
+                       TESTING=os.path.join(new_dir, 'testing', labels[i]),
+                       SPLIT_SIZE=0.8)
+
+    def multi_process_dataset(self, new_dir, labels, src_directories):
+        self.make_data_directories(new_dir, labels)
+        processes = []
+        for i in range(len(labels)):
+            training_path = os.path.join(new_dir, 'training', labels[i])
+            testing_path = os.path.join(new_dir, 'testing', labels[i])
+            p = mp.Process(target=self.split_data, args=(src_directories[i], training_path, testing_path, 0.9))
+            processes.append(p)
+            p.start()
+
+        for process in processes:
+            process.join()
+
+
+
